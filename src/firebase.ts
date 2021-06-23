daa79542b7fb6ca843871ed27c75f153f5f17c10
nemutas <inohiro.focru@gmail.com>
2021-06-23 22:54:00 +0900

    firebaseのconfigを切り分けた

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,31 @@
-import firebase from 'firebase';
+import 'firebase/database';
+import firebase from 'firebase/app';
+
+const {
+	REACT_APP_FIREBASE_API_KEY,
+	REACT_APP_FIREBASE_AUTH_DOMAIN,
+	REACT_APP_FIREBASE_DATABASE_URL,
+	REACT_APP_FIREBASE_PROJECT_ID,
+	REACT_APP_FIREBASE_STORAGE_BUCKET,
+	REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	REACT_APP_FIREBASE_APP_ID
+} = process.env;
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyDb8IH6dewMp9SaUGaxifsUzWV3KT28VJ4',
-	authDomain: 'nemutas-idobatakaigi.firebaseapp.com',
-	databaseURL: 'https://nemutas-idobatakaigi-default-rtdb.asia-southeast1.firebasedatabase.app',
-	projectId: 'nemutas-idobatakaigi',
-	storageBucket: 'nemutas-idobatakaigi.appspot.com',
-	messagingSenderId: '853703029424',
-	appId: '1:853703029424:web:b5d52fa8b5c74cd62e4175'
+	apiKey: REACT_APP_FIREBASE_API_KEY,
+	authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: REACT_APP_FIREBASE_APP_ID
 };
 
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const database = firebase.database();
-const messageRef = database.ref('messages');
+const realtimeDB = firebaseApp.database();
+const messagesRef = realtimeDB.ref('messages');
 
 export const pushMessage = (name: string, text: string) => {
-	messageRef.push({ name, text });
+	messagesRef.push({ name, text });
 };
